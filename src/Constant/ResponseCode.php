abb9b727f11b44a27024de1d74c590d2d09b28f8
lmh <lmh@weiyian.com>
2020-11-11 21:09:38 +0800

    增加响应编码

--- src/Constant/ResponseCode.php
+++ src/Constant/ResponseCode.php
@@ -11,10 +11,18 @@ class ResponseCode
      * 表示交易成功。同步交易，指业务办理成功；异步交易，指交易成功 但业务办理结果未知，
      */
     public const SUCCESS = '000000';
-
     /**
      * 则表示开户处理中，需要通过 T1002 查询开户结果
      */
     public const E3010 = 'E3010';
+    /**
+     *  远程通信失败
+     */
+    public const SDER02 = 'SDER02';
+    /**
+     * 合作方不支持该交易
+     */
+    public const SDER07 = 'SDER07';
+
 
 }
