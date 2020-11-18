313c5d8c32384994816f21deb64318d083c0aa3b
lmh <lmh@weiyian.com>
2020-11-18 20:00:36 +0800

    退款类型

---
+++ src/Constant/RefundDtrcd.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: lmh <lmh@weiyian.com>
+ * Date: 2020/11/18
+ * Time: 下午7:54
+ */
+
+namespace Lmh\Cpcn\Constant;
+
+
+class RefundDtrcd
+{
+    /**
+     * @var string UIN:渠道入金
+     */
+    public const UIN = '';
+    /**
+     * @var string 订单支付  3001-3007
+     */
+    public const PAY = '';
+    /**
+     * @var string 入金支付  3061-3069
+     */
+    public const INPAY = '';
+}
