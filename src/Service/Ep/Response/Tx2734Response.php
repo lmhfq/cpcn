66f027e322f8175b8742820fe9ad5e3e3d0c28d1
lmh <lmh@weiyian.com>
2022-02-17 21:01:22 +0800

    支付接口

---
+++ src/Service/Ep/Response/Tx2734Response.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: lmh <lmh@weiyian.com>
+ * Date: 2022/2/17
+ * Time: 下午9:00
+ */
+
+namespace Lmh\Cpcn\Service\Ep\Response;
+
+
+class Tx2734Response extends BaseResponse
+{
+
+}
--- src/Service/Ep/Response/Tx7703Response.php
+++ src/Service/Ep/Response/Tx7703Response.php
@@ -10,7 +10,63 @@ declare(strict_types=1);
 namespace Lmh\Cpcn\Service\Ep\Response;
 
 
+use Lmh\Cpcn\Constant\TxResponseCode;
+use Lmh\Cpcn\Support\ArrayUtil;
+
 class Tx7703Response extends BaseResponse
 {
+    /**
+     * @var string
+     */
+    protected $phoneNumber;
+    /**
+     * @var int
+     */
+    protected $status;
+
+    /**
+     * @return string
+     */
+    public function getPhoneNumber(): string
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param string $phoneNumber
+     */
+    public function setPhoneNumber(string $phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus(int $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @param string $message
+     * @author lmh
+     */
+    public function handle(string $message)
+    {
+        parent::handle($message);
+        if ($this->code == TxResponseCode::SUCCESS) {
+            $this->phoneNumber = ArrayUtil::get('PhoneNumber', $this->responseBody);
+            $this->status = ArrayUtil::get('Status', $this->responseBody);
+        }
+    }
 
 }
