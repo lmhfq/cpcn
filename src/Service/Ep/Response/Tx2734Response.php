c7a2a1c9c4b6e97dda921a6994368fd9f164fb3b
lmh <lmh@weiyian.com>
2022-02-17 21:24:52 +0800

    支付接口

--- src/Service/Ep/Response/Tx2734Response.php
+++ src/Service/Ep/Response/Tx2734Response.php
@@ -10,7 +10,41 @@ declare(strict_types=1);
 namespace Lmh\Cpcn\Service\Ep\Response;
 
 
+use Lmh\Cpcn\Constant\TxResponseCode;
+use Lmh\Cpcn\Support\ArrayUtil;
+
 class Tx2734Response extends BaseResponse
 {
+    /**
+     * @var int
+     */
+    protected $status;
 
+    /**
+     * @return int
+     */
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus(int $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @param string $message
+     * @author lmh
+     */
+    public function handle(string $message)
+    {
+        parent::handle($message);
+        if ($this->code == TxResponseCode::SUCCESS) {
+            $this->status = ArrayUtil::get('Status', $this->responseBody);
+        }
+    }
 }
